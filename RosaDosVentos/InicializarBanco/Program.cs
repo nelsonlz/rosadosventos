4a0bd2e1dadf13fd765e18763de9fb763b76d655
luizhlb <lbortolloto@outlook.com>
2015-06-27 18:04:03 -0300

    cofiguração do banco de dados

---
+++ RosaDosVentos/InicializarBanco/Program.cs
@@ -0,0 +1,31 @@
+﻿using Entidades;
+using MySql.Data.MySqlClient;
+using Persistencia;
+using Regras;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InicializarBanco
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cadastroUsuario = new CadastroUsuario();
+
+            cadastroUsuario.Incluir(new Usuario { Ativo = true, DataCriacao = DateTime.Now, Email = "teste@teste.com", Login = "admin", Nome = "Admin", Nivel = "A", Senha = "teste" });
+            //Inserir aqui os cadastros que devem estar no banco em sua criação
+
+            foreach (var item in cadastroUsuario.RetornaTodos())
+            {
+                Console.WriteLine("ID = {0}, Nome = {1}, Nivel = {2}", item.Id, item.Nome, item.NivelEnum);
+            }
+            Console.ReadLine();
+
+           
+        }
+    }
+}
--- RosaDosVentos/Persistencia/SIteContexto.cs
+++ RosaDosVentos/Persistencia/SIteContexto.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Entidades;
+using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Data.Entity.ModelConfiguration.Conventions;
@@ -8,13 +9,15 @@ using System.Threading.Tasks;
 
 namespace Persistencia
 {
+    //[DbConfigurationType(typeof(MySqlEFConfiguration))]
     public  class SiteContexto : DbContext
     {
         private readonly bool _excluir = false;
-        
 
         //Inicializar as classes que seram espelhadas em tabelas
         // ex: public DbSet<NomeDaClasse> NomeDaTabela { get; set; }
+
+        public DbSet<Usuario> Usuarios { get; set; }
 
         public SiteContexto(bool exclui)
         {
